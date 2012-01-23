9f96803c18b6066a2a71accae1e05335445cbedf
Helen.Fornazier@gmail.com <Helen.Fornazier@gmail.com@a9fdb26a-8d04-983f-416e-fb5877f047f8>
2012-01-23 10:18:25 +0000

    float_copro.sv primeira implementacao

--- LM32_SYNTH_COPRO/copro_src/float_copro.sv
+++ LM32_SYNTH_COPRO/copro_src/float_copro.sv
@@ -1,6 +1,9 @@
 // Squelette du coprocesseur flottant
-module float_copro(
-               input logic clk,
+module float_copro #(
+               parameter t_mult = 5,
+               parameter t_add = 3,
+               parameter t_sub = 2)  
+               (input logic clk,
                input logic copro_valid,
                input logic copro_accept,
                input logic [10:0] copro_opcode,
@@ -24,6 +27,54 @@ module float_copro(
    //////////////////////////////////////////////////
    // Compléter en rajoutant registres et contrôle //
    //////////////////////////////////////////////////
-   
+
+   int COUNT;
+
+   initial
+   begin
+      COUNT = 0;
+      copro_complete = 0;
+   end
+
+   always_ff @ (posedge clk)
+   begin
+      if (copro_accept) begin
+         copro_complete = 0;
+      end
+      else if (!copro_complete) begin
+              if (COUNT == 0 && copro_valid) begin
+                 op0 = copro_op0;
+                 op1 = copro_op1;
+                 opcode = copro_opcode;
+                 COUNT = COUNT + 1;
+              end
+              else if (COUNT) case (opcode)
+                //somme
+                 11'd0:
+                    if (COUNT >= t_add) begin
+                       copro_complete = 1;
+                       copro_result = resultat;
+                       COUNT = 0;
+                    end  
+                    else COUNT = COUNT + 1;
+                //soustraction
+                 11'd1:
+                    if (COUNT >= t_sub) begin
+                       copro_complete = 1;
+                       copro_result = resultat;
+                       COUNT = 0;
+                    end  
+                    else COUNT = COUNT + 1;
+                //multiplication
+                 11'd2:
+                    if (COUNT >= t_mult) begin
+                       copro_complete = 1;
+                       copro_result = resultat;
+                       COUNT = 0;
+                    end  
+                    else COUNT = COUNT + 1;
+              endcase
+      end
+   end
 endmodule
 
