e09ba2906840e56438f185d5fe21ff160ef0393e
thalitafdrumond@gmail.com <thalitafdrumond@gmail.com@a9fdb26a-8d04-983f-416e-fb5877f047f8>
2012-01-27 10:41:36 +0000

    Conversoes float OK

--- LM32_SYNTH_COPRO/copro_src/float_pack.sv
+++ LM32_SYNTH_COPRO/copro_src/float_pack.sv
@@ -1,29 +1,29 @@
 package float_pack; 
-// Ajouter ici les définition des fonctions
-// utilisée par votre coprocesseur
+   // Ajouter ici les définition des fonctions
+   // utilisée par votre coprocesseur
 `ifdef TB_MANT_SIZE
-  parameter Nm = `TB_MANT_SIZE ;
+   parameter Nm = `TB_MANT_SIZE ;
 `else
-  parameter Nm = 23 ;
+   parameter Nm = 23 ;
 `endif
-
+   
 `ifdef TB_EXP_SIZE
-  parameter Ne = `TB_EXP_SIZE ;
+   parameter Ne = `TB_EXP_SIZE ;	
 `else
-  parameter Ne = 8;
+   parameter Ne = 8;
 `endif
 
-typedef struct packed {
-        logic s;
-        logic [Ne:0]exp;
-        logic [Nm:0]mant;
-} float;
+   typedef struct packed {
+      logic 	  s;
+      logic [Ne-1:0] exposant;
+      logic [Nm-1:0] mantisse;
+   } float;
 
-typedef struct packed {
-        logic s;
-        logic [8:0]exp;
-        logic [23:0]mant;
-} float_ieee;
+   typedef struct  packed {
+      logic 	   s;
+      logic [7:0]  exposant;
+      logic [22:0] mantisse;
+   } float_ieee;
 
 /*-------- Calcul Flotant --------*/
 
@@ -54,12 +54,34 @@ endfunction
 
 /*---------- Convertion ----------*/
 
-function float_ieee real2float_ieee(shortreal number);
-        return $shortrealtobits(number);
-endfunction
+   function float_ieee real2float_ieee(shortreal number);
+      return $shortrealtobits(number);
+   endfunction
 
-function shortreal float_ieee2real(float_ieee number);
-        return $bitstoshortreal(number);
-endfunction
+   function shortreal float_ieee2real(float_ieee number);
+      return $bitstoshortreal(number);
+   endfunction // float_ieee2real
+
+   function float real2float (shortreal number);
+      float_ieee nieee;
+      float n;
+      nieee = real2float_ieee (number);  
+      //truncate
+      n.s = nieee.s;
+      n.mantisse[Nm-1:0] = nieee.mantisse[22:23-Nm];      
+      n.exposant[Nm-1:0] = nieee.exposant[7:0] - (2**(Ne-1)) + 128;
+      return n;    
+   endfunction // real2float
+   
+   function shortreal float2real (float n);
+      float_ieee nieee;
+      shortreal  number;
+      nieee.s = n.s;      
+      nieee.mantisse[22:0] = 0;
+      nieee.mantisse[22:23-Nm] = n.mantisse[Nm-1:0];      
+      nieee.exposant[7:0] = n.exposant[Ne-1:0] + (2**(Ne-1)) - 128;
+      number = float_ieee2real(nieee); 
+      return number;    
+   endfunction // real2float
 
 endpackage : float_pack
