a314ff3603fcba030ab152f403d01c56ad847920
Helen.Fornazier@gmail.com <Helen.Fornazier@gmail.com@a9fdb26a-8d04-983f-416e-fb5877f047f8>
2012-01-27 07:58:28 +0000

    Definicao typedef de float e float_ieee

--- LM32_SYNTH_COPRO/copro_src/float_pack.sv
+++ LM32_SYNTH_COPRO/copro_src/float_pack.sv
@@ -1,5 +1,29 @@
 package float_pack; 
 // Ajouter ici les définition des fonctions
 // utilisée par votre coprocesseur
+`ifdef TB_MANT_SIZE
+  parameter Nm = `TB_MANT_SIZE ;
+`else
+  parameter Nm = 23 ;
+`endif
+
+`ifdef TB_EXP_SIZE
+  parameter Ne = `TB_EXP_SIZE ;
+`else
+  parameter Ne = 8;
+`endif
+
+typedef struct packed {
+        logic s;
+        logic [Ne:0]exp;
+        logic [Nm:0]mant;
+} float;
+
+typedef struct packed {
+        logic s;
+        logic [8:0]exp;
+        logic [23:0]mant;
+} float_ieee;
+
 endpackage : float_pack
    
